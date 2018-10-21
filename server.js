d1cd7770913e7fae6c0b6109c3f5ed35c7fe4151
Glitch (serious-error) <none>
2018-10-21 10:26:21 +0000

    🍻🛢 Checkpoint ./server.js:1715671/780

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// get IP address, preferred languages, and system info
+app.get("/api/whoami", function (req, res) {
+  
+  // let date = new Date(req.params.date_string);
+  // res.json({
+  //   "unix": date.getTime(), "utc": date.toUTCString()
+  // });
+  
+  res.json({
+    // "ipaddress": "::ffff:159.20.14.100",
+    // "language": "en-US,en;q=0.5",
+    // "software": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+  });
+});
+
 
 
 // listen for requests :)
